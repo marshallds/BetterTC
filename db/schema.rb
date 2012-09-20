78a346ec753f09cf4a3c70785c1dd07190d80516
Marshall Smith <marshall.ds@gmail.com>
2012-09-20 17:29:25 -0600

    finish adding tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920225258) do
+ActiveRecord::Schema.define(:version => 20120920230233) do
 
   create_table "employees", :force => true do |t|
     t.string   "firstname"
@@ -28,8 +28,16 @@ ActiveRecord::Schema.define(:version => 20120920225258) do
     t.text     "log"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "jobs"
   end
 
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
+
+  create_table "jobs", :force => true do |t|
+    t.string   "name"
+    t.boolean  "active"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
