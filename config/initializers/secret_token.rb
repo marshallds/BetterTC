e0940fc76fdaf5cc9644ab63c42325b11ed380d3
Marshall Smith <marshall.ds@gmail.com>
2012-09-20 10:02:37 -0600

    fix bad add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterTC::Application.config.secret_token = '8fa35d80bae32080dcf1af6556ff602a62e7888446cddfe46ee08159b84a753032e0d368bde0f40913e31d2b89244aa7b5b7e77dab84fdd14b55e3d3dba04944'
