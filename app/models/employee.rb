6867a58d20c7a5f738d9fa213db6a7055563d60a
Marshall Smith <marshall.ds@gmail.com>
2012-09-21 10:37:24 -0600

    fix stupid table names

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :username
+  has_many :events
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
   attr_accessible :active, :name
+  has_many :events
 end
