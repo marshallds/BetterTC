1a0096801e7dd0c7521665ae4c2fa91cedf9ddfc
Marshall Smith <marshall.ds@gmail.com>
2012-09-21 18:06:16 -0600

    report displays total hours by jobs

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,21 @@
 module EventsHelper
+	def job_totals 
+		jobs = Job.where({:active => true})
+
+
+		jobs.each do |job|
+			job.hours = 0		
+			Employee.all.each do |employee|
+				job_users_events = employee.events.where :job_id => job.id
+				job_users_events.each do |event|
+					job.hours -= event.punchtime.to_i if event.punch_type == "IN"
+					job.hours += event.punchtime.to_i if event.punch_type == "OUT" 
+						
+				end
+			end
+			job.hours /= 3600.0
+
+		end
+		jobs
+	end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ActiveRecord::Base
-  attr_accessible :active, :name
+  attr_accessible :active, :name, :hours
   has_many :events
+  attr_accessor :hours 
 end
